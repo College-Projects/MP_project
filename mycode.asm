5d1caeddc342e5f3b8014ed6c5e1f917281c7c54
alaa5radwan <66218719+alaa5radwan@users.noreply.github.com>
2022-12-08 21:50:20 +0200

    Update mycode.asm

--- mycode.asm
+++ mycode.asm
@@ -12,5 +12,16 @@ banner5 db  '|  |  |  |  |  _  |    \|   |   |  _  |$'
 banner6 db  '|  |  |  |  |  |  |  .  \   |   |  |  |$'
 banner7 db  '|__|  |__|__|__|__|__|\_|___|___|__|__|$'
 
+opt1 db 10,13, '1. Panadol - 20b$'
+opt2 db 10,13, '2. Paracetamol - 30b$'
+opt3 db 10,13, '3. Cleritek - 20b$'
+opt4 db 10,13, '4. Aspirin - 50b$'
+opt5 db 10,13, '5. Brufen - 100b$'
+opt6 db 10,13, '6. Surbex Z - 80b$'
+opt7 db 10,13, '7. Arinac - 40b$'
+opt8 db 10,13, '8. Sinopharm Vaccine - 10b$'
+opt9 db 10,13, '9. Pfizer Vaccine - 20b$'
 
+   
+   
 .code 
