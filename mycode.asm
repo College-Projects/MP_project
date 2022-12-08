6a345e9f81cc464780bf2ab9edbaea3a15f4bf68
alaa5radwan <66218719+alaa5radwan@users.noreply.github.com>
2022-12-08 21:45:55 +0200

    Add files via upload

---
+++ mycode.asm
@@ -0,0 +1,16 @@
+.model small
+
+.stack 100h
+
+.data
+   
+banner1 db  ' ____  __ __  ____ ____  ___ ___  ____ $'
+banner2 db  '|    \|  |  |/    |    \|   |   |/    |$'
+banner3 db  '|  o  )  |  |  o  |  D  ) _   _ |  o  |$'
+banner4 db  '|   _/|  _  |     |    /|  \_/  |     |$'
+banner5 db  '|  |  |  |  |  _  |    \|   |   |  _  |$'
+banner6 db  '|  |  |  |  |  |  |  .  \   |   |  |  |$'
+banner7 db  '|__|  |__|__|__|__|__|\_|___|___|__|__|$'
+
+
+.code 
